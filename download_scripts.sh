3443d5823f3f1de9195c2cd73bb3973f44eafc25
marevalo10 <marevalo10@gmail.com>
2021-01-19 02:50:08 +1100

    Add files via upload

---
+++ download_scripts.sh
@@ -0,0 +1,19 @@
+echo "Downloading and Installing important dotfiles"
+wget https://raw.githubusercontent.com/marevalo10/hackfiles/main/dotfiles_mod.zip
+unzip  dotfiles_mod.zip
+cd dotfiles
+chmod +x *.sh
+sudo ./install.sh
+echo "Copying the tmux logging files to ~/tmux-logging"
+cp -R tmux-logging ~/
+cd ..
+echo "*********************************************************"
+
+echo "#Downloading recon scripts"
+wget https://raw.githubusercontent.com/marevalo10/hackfiles/main/run_scripts_tmux.sh
+chmod +x run_scripts_tmux.sh
+wget https://raw.githubusercontent.com/marevalo10/hackfiles/main/scripts_recon.zip
+unzip scripts_recon.zip
+cd scripts_recon
+chmod +x *.sh
+echo "#Files in Scripts_recon"
