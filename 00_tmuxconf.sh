d0b61aaabedb2023789899b07be3a44e2b6f4b96
M@rc14n0 <marevalo10@gmail.com>
2021-08-14 20:58:40 +1000

    tmux script

---
+++ 00_tmuxconf.sh
@@ -0,0 +1,14 @@
+echo "****************************************************************"
+echo "Installing the shell and tmux improvements scripts for kali user "
+echo "****************************************************************"
+    echo "Downloading and Installing dotfiles"
+    wget https://raw.githubusercontent.com/marevalo10/hackfiles/main/dotfiles_mod.zip -O dotfiles_mod.zip
+    unzip  dotfiles_mod.zip
+    cd dotfiles
+    chmod +x *.sh
+    sudo ./install.sh
+    echo "Copying the tmux logging files to ~/tmux-logging"
+    cp -R tmux-logging ~/
+    cd ..
+    rm dotfiles_mod.zip
+    echo "tmux configured to support mouse and logs..."
