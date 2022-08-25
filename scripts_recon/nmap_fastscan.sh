9497b30feedba2320bcc6d835d803d2562872930
marevalo10 <marevalo10@gmail.com>
2022-08-25 12:07:37 +1000

    0 and 1 slightly improved

--- scripts_recon/nmap_fastscan.sh
+++ scripts_recon/nmap_fastscan.sh
@@ -5,6 +5,9 @@ rm portsbyhostTCP.csv
 grep "open/tcp" nmap_fastallports_ipsext.gnmap > temprawtcp.txt
 for ipadd in $(grep "open/tcp" nmap_fastallports_ipsext.gnmap | awk '{print $2}'); do
         tcpports=`awk FNR==$line temprawtcp.txt | grep -o  -E "\b[0-9]{1,5}/open"  | sed 's/\/open//g' | awk -vORS=, '{ print  }'`;
-            echo $ipadd $tcpports | tee -a portsbyhostTCP.csv   
-            line=$(($line+1));                            
+        echo $ipadd $tcpports | tee -a portsbyhostTCP.csv   
+        line=$(($line+1));                            
 done 
+#Delete duplicated IP's
+cp portsbyhostTCP.csv portsbyhostTCP.csv.tmp
+cat portsbyhostTCP.csv.tmp | sort -u |tee portsbyhostTCP2.csv
